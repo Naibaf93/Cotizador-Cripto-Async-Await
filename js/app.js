4ff2b82876a2e2b832f0f3471b2b159064db6d19
Fabián Huertas <miloh9318@hotmail.com>
2024-12-06 15:03:19 -0500

    haciendo un cambio en los label del HTML creando la funcion selectCriptomonedas haciendo que los label generen la 10 criptos  mas populares

--- js/app.js
+++ js/app.js
@@ -16,5 +16,16 @@ function consultarCriptomonedas() {
     fetch(url)
         .then( respuesta => respuesta.json() )
         .then( resultado => obtenerCriptomonedas(resultado.Data))
-        .then( criptomonedas => console.log(criptomonedas))
+        .then( criptomonedas => selectCriptomonedas(criptomonedas))
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+    })
 }
