155d11c56878b788c90aab1dac2dcf90f9d39d60
Fabián Huertas <miloh9318@hotmail.com>
2024-12-06 14:31:29 -0500

    creando el add eventListener y primera funcion consultarCriptomonedas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+})
+
+function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=COP';
+}
